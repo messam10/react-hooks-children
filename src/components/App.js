43f842e3d9a2294ff9b6120244a0ef5700ef1a18
messam10 <memaq1998.10.7@gmail.com>
2023-03-27 00:30:51 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,32 +4,12 @@ import Container from "./Container";
 function App() {
   return (
     <div className="App">
-      <Container header="Example header" direction="horizontal">
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
-        <div>This is an example!</div>
+      <Container header="Example header" direction="horizontal" children="This is an example!">
       </Container>
-      <Container header="Example header" textPosition="center">
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
-        <img
-          alt="turtle"
-          src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
-        />
+      <Container header="Example header" textPosition="center" children={<img
+        alt="turtle"
+        src="https://pbs.twimg.com/profile_images/3022993403/fdae67d800527328cee7a222f048dbbd.jpeg"
+      />}>
       </Container>
     </div>
   );
